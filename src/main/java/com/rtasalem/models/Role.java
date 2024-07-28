bcd7e739b26f664dc1f3165ad4e81ca8712f4059
Rana Salem <ranatasalem@gmail.com>
2024-07-28 22:05:14 +0100

    install lombok

---
+++ src/main/java/com/rtasalem/models/Role.java
@@ -0,0 +1,40 @@
+package com.rtasalem.models;
+
+import org.springframework.security.core.GrantedAuthority;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "roles")
+@Getter
+@Setter
+@NoArgsConstructor(force = true)
+@RequiredArgsConstructor
+@AllArgsConstructor
+public class Role implements GrantedAuthority {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "role_id")
+    private Integer roleId;
+
+    @NonNull
+    private String authority;
+
+    @Override
+    public String getAuthority() {
+        return authority;
+    }
+
+}
